9bfa41c6c5e314029d8e015850c0ff1211b44d10
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-03-27 21:39:47 -0700

    updates cljsjs package to one that works with production builds

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject akjetma/dmp-clj "0.1.1"
+(defproject akjetma/dmp-clj "0.1.3"
   :author "Adam Jetmalani <https://akjetma.github.io>"
   :description "comprehensive clojure/clojurescript wrapper for google's diff match patch library"
   :url "http://github.com/akjetma/dmp-clj"  
@@ -10,7 +10,7 @@
   :min-lein-version "2.5.3"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
-                 [cljsjs/google-diff-match-patch "20121119-0"]]
+                 [cljsjs/google-diff-match-patch "20121119-1"]]
   :plugins [[lein-cljsbuild "1.1.2"]]
   :clean-targets ^{:protect false} ["target" "test.js"]
   :source-paths ["src"]
